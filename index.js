ffc216139ed499c20a3e55b97889562f59fade93
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-11-25 08:23:01 +0100

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+var session = require('express-session');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/assets', express.static('assets'));
+app.use(session({
+    secret: process.env.SESSION_SECRET
+}));
+
+const routes = require('./modules/torpek');
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
